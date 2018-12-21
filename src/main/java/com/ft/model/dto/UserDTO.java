706527a9674ecc2efcc701876bed54caad8aaea9
shichunyang <shichunyang@guazi.com>
2018-12-21 20:38:29 +0800

    swagger

--- src/main/java/com/ft/model/dto/UserDTO.java
+++ src/main/java/com/ft/model/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.ft.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ft.util.DateUtil;
 import com.ft.web.constant.SwaggerConstant;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -11,18 +12,20 @@ import lombok.Data;
  *
  * @author shichunyang
  */
-@ApiModel(value = "user对象", description = "用户信息")
+@ApiModel(value = "用户对象", description = "存储用户相关信息")
 @Data
 public class UserDTO {
 
-	@ApiModelProperty(name = "id", value = "用户id", required = true, dataType = SwaggerConstant.DATA_TYPE_INT, example = "0")
+	@ApiModelProperty(value = "用户id", required = true, dataType = SwaggerConstant.DATA_TYPE_INT, example = "0")
 	private Integer id;
 
-	@ApiModelProperty(name = "name", value = "姓名", required = true, dataType = SwaggerConstant.DATA_TYPE_STRING, example = "春阳")
+	@ApiModelProperty(value = "姓名", required = true, dataType = SwaggerConstant.DATA_TYPE_STRING, example = "春阳")
 	private String name;
 
+	@ApiModelProperty(value = "年龄", dataType = SwaggerConstant.DATA_TYPE_INT, example = "0")
 	private Integer age;
 
+	@ApiModelProperty(value = "创建时间", required = true, dataType = SwaggerConstant.DATA_TYPE_STRING, example = DateUtil.DEFAULT_DATE_FORMAT)
 	@JsonProperty("crt_time")
 	private String crtTime;
 }
