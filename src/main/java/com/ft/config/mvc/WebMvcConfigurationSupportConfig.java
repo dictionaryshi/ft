634c33110b52ccf92b186e1f120fbb2044edb2dd
shichunyang <shichunyang@guazi.com>
2019-03-19 18:05:57 +0800

    diff

---
+++ src/main/java/com/ft/config/mvc/WebMvcConfigurationSupportConfig.java
@@ -0,0 +1,13 @@
+package com.ft.config.mvc;
+
+import com.ft.web.plugin.BaseWebMvcConfigurationSupport;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * WebMvcConfigurationSupportConfig
+ *
+ * @author shichunyang
+ */
+@Configuration
+public class WebMvcConfigurationSupportConfig extends BaseWebMvcConfigurationSupport {
+}
