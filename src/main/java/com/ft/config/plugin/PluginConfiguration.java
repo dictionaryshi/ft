ba6c69d6031244334ead34e106f05b18cd68ff27
shichunyang <shichunyang@guazi.com>
2019-03-21 11:28:27 +0800

    redis kafka auto config

--- src/main/java/com/ft/config/plugin/PluginConfiguration.java
+++ src/main/java/com/ft/config/plugin/PluginConfiguration.java
@@ -1,7 +1,5 @@
 package com.ft.config.plugin;
 
-import com.ft.redis.plugin.KafkaAop;
-import com.ft.redis.plugin.RedisAop;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -24,21 +22,5 @@ public class PluginConfiguration {
 	public LoginAop loginAop() {
 		log.info("插件初始化==>LoginAop");
 		return new LoginAop();
-	}
-
-	/**
-	 * Redis插件
-	 */
-	@Bean
-	@ConditionalOnMissingBean(RedisAop.class)
-	public RedisAop redisAop() {
-		log.info("插件初始化==>RedisAop");
-		return new RedisAop();
-	}
-
-	@Bean
-	@ConditionalOnMissingBean(KafkaAop.class)
-	public KafkaAop kafkaAop() {
-		return new KafkaAop();
 	}
 }
