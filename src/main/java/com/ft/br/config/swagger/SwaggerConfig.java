01e77aaf3f727ba3f7f86ef72821f8dc63b3567b
shichunyang <shichunyang@guazi.com>
2019-03-25 17:26:00 +0800

    Swagger config

---
+++ src/main/java/com/ft/br/config/swagger/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.ft.br.config.swagger;
+
+import com.ft.web.util.SwaggerUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * SwaggerConfig
+ *
+ * @author shichunyang
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket createRestApi() {
+		return SwaggerUtil.docket(
+				SwaggerUtil.apiInfo("温馨家具API", "前后端分离参考文档", "http://www.baidu.com", "史春阳", "http://www.google.com", "903031015@qq.com", "V1.0"),
+				"wx",
+				PathSelectors.regex("/api.*")
+		);
+	}
+}
