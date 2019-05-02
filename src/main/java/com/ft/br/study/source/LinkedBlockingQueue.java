b8f7e485639e3b305940ac2b47c38df9fd28ed31
shichunyang <shichunyang@guazi.com>
2019-05-02 16:02:35 +0800

    queue

--- src/main/java/com/ft/br/study/source/LinkedBlockingQueue.java
+++ src/main/java/com/ft/br/study/source/LinkedBlockingQueue.java
@@ -191,4 +191,54 @@ public class LinkedBlockingQueue<E> {
 			putLock.unlock();
 		}
 	}
+
+	public void put(E e) throws InterruptedException {
+		if (e == null) {
+			throw new NullPointerException();
+		}
+		int c = -1;
+		Node<E> node = new Node<E>(e);
+		final ReentrantLock putLock = this.putLock;
+		final AtomicInteger count = this.count;
+		putLock.lockInterruptibly();
+		try {
+			while (count.get() == capacity) {
+				notFull.await();
+			}
+			enqueue(node);
+			c = count.getAndIncrement();
+			if (c + 1 < capacity) {
+				notFull.signal();
+			}
+		} finally {
+			putLock.unlock();
+		}
+		if (c == 0) {
+			signalNotEmpty();
+		}
+	}
+
+	public E take() throws InterruptedException {
+		E x;
+		int c = -1;
+		final AtomicInteger count = this.count;
+		final ReentrantLock takeLock = this.takeLock;
+		takeLock.lockInterruptibly();
+		try {
+			while (count.get() == 0) {
+				notEmpty.await();
+			}
+			x = dequeue();
+			c = count.getAndDecrement();
+			if (c > 1) {
+				notEmpty.signal();
+			}
+		} finally {
+			takeLock.unlock();
+		}
+		if (c == capacity) {
+			signalNotFull();
+		}
+		return x;
+	}
 }
