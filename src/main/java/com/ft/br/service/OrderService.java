b74699428e52c63162d63e3243f5e5065e337034
shichunyang <shichunyang@guazi.com>
2019-06-28 19:06:03 +0800

    delete hystrix

--- src/main/java/com/ft/br/service/OrderService.java
+++ src/main/java/com/ft/br/service/OrderService.java
@@ -28,6 +28,7 @@ import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
