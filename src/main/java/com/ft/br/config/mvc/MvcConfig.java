006a564f307832b39985b12949c7e36ffe19fb96
shichunyang <shichunyang@guazi.com>
2019-06-30 16:10:48 +0800

    抽取

--- src/main/java/com/ft/br/config/mvc/MvcConfig.java
+++ src/main/java/com/ft/br/config/mvc/MvcConfig.java
@@ -1,14 +1,10 @@
 package com.ft.br.config.mvc;
 
 import com.ft.web.plugin.MailUtil;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.util.unit.DataSize;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
-import javax.servlet.MultipartConfigElement;
 
 /**
  * MvcConfig
@@ -17,19 +13,6 @@ import javax.servlet.MultipartConfigElement;
  */
 @Configuration
 public class MvcConfig {
-	/**
-	 * 文件上传配置
-	 */
-	@Bean
-	public MultipartConfigElement multipartConfigElement() {
-		MultipartConfigFactory factory = new MultipartConfigFactory();
-		// 单个文件最大
-		factory.setMaxFileSize(DataSize.parse("5MB"));
-		/// 设置总上传数据总大小
-		factory.setMaxRequestSize(DataSize.parse("20MB"));
-		return factory.createMultipartConfig();
-	}
-
 	/**
 	 * 开启WebSocket支持
 	 */
