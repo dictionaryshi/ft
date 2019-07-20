11cedbdf1f9ddb25b150bdce677a00a2b538d2f1
shichunyang <shichunyang@guazi.com>
2019-07-20 14:18:47 +0800

    LogMapper.java

--- src/main/java/com/ft/br/dao/LogMapper.java
+++ src/main/java/com/ft/br/dao/LogMapper.java
@@ -1,10 +1,7 @@
 package com.ft.br.dao;
 
-import com.ft.dao.stock.model.LogDO;
-import com.ft.db.constant.DbConstant;
-import org.apache.ibatis.annotations.InsertProvider;
+import com.ft.dao.stock.mapper.LogDOMapper;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.SelectKey;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,15 +11,5 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @Mapper
-public interface LogMapper {
-
-	/**
-	 * 插入日志
-	 *
-	 * @param log 日志
-	 * @return 1:插入成功
-	 */
-	@SelectKey(statement = DbConstant.SELECT_LAST_INSERT_ID, keyColumn = "id", keyProperty = "id", resultType = Integer.class, before = false)
-	@InsertProvider(type = LogSqlProvider.class, method = "insertSelective")
-	int insertSelective(LogDO log);
+public interface LogMapper extends LogDOMapper {
 }
