9c21ed48722fb040f6854cd2a56847f69681291b
shichunyang <shichunyang@guazi.com>
2019-07-20 14:26:32 +0800

    OrderMapper.java

--- src/main/java/com/ft/br/dao/OrderMapper.java
+++ src/main/java/com/ft/br/dao/OrderMapper.java
@@ -2,9 +2,10 @@ package com.ft.br.dao;
 
 import com.ft.br.model.dto.OrderDTO;
 import com.ft.br.model.vo.OrderVO;
-import com.ft.dao.stock.model.OrderDO;
+import com.ft.dao.stock.mapper.OrderDOMapper;
 import com.ft.util.StringUtil;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.SelectProvider;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -16,34 +17,9 @@ import java.util.List;
  */
 @Component
 @Mapper
-public interface OrderMapper {
+public interface OrderMapper extends OrderDOMapper {
 
 	class SqlBuilder {
-		public String update(OrderDO orderDO) {
-			StringBuilder sb = new StringBuilder("update `order` ");
-			sb.append("set `operator` = ").append(orderDO.getOperator());
-			if (orderDO.getStatus() != null) {
-				sb.append(", `status` = ").append(orderDO.getStatus());
-			}
-			if (!StringUtil.isNull(orderDO.getUsername())) {
-				sb.append(", `username` = '").append(orderDO.getUsername()).append("'");
-			}
-			if (!StringUtil.isNull(orderDO.getPhone())) {
-				sb.append(", `phone` = '").append(orderDO.getPhone()).append("'");
-			}
-			if (!StringUtil.isNull(orderDO.getAddress())) {
-				sb.append(", `address` = '").append(orderDO.getAddress()).append("'");
-			}
-			if (orderDO.getTotalAmount() != null) {
-				sb.append(", `total_amount` = ").append(orderDO.getTotalAmount());
-			}
-			if (!StringUtil.isNull(orderDO.getRemark())) {
-				sb.append(", `remark` = '").append(orderDO.getRemark()).append("'");
-			}
-			sb.append(" where `id` = '").append(orderDO.getId()).append("'");
-			return sb.toString();
-		}
-
 		private String query(OrderDTO orderDTO) {
 			StringBuilder sb = new StringBuilder();
 			sb.append("where 1 = 1 ");
@@ -79,34 +55,6 @@ public interface OrderMapper {
 			return sql;
 		}
 	}
-
-	/**
-	 * 添加订单信息
-	 *
-	 * @param orderDO 订单信息
-	 * @return 1:添加成功
-	 */
-	@Insert("insert into `order` (`id`, `operator`, `status`, `username`, `phone`, `address`, `total_amount`, `remark`) " +
-			"values (#{id}, #{operator}, #{status}, #{username}, #{phone}, #{address}, #{totalAmount}, #{remark})")
-	int insert(OrderDO orderDO);
-
-	/**
-	 * 修改订单信息
-	 *
-	 * @param orderDO 订单信息
-	 * @return 1:修改成功
-	 */
-	@UpdateProvider(type = SqlBuilder.class, method = "update")
-	int update(OrderDO orderDO);
-
-	/**
-	 * 根据主键查询订单信息
-	 *
-	 * @param id 主键
-	 * @return 订单信息
-	 */
-	@Select("select * from `order` where `id` = #{id}")
-	OrderVO getOrderById(@Param("id") Long id);
 
 	/**
 	 * 查询符合条件的数量
