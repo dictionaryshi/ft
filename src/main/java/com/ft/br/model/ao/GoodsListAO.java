046bdd3d77393631ff0239a407fd1e089c08a859
shichunyang <shichunyang@guazi.com>
2019-07-20 11:58:38 +0800

    GoodsListAO.java

---
+++ src/main/java/com/ft/br/model/ao/GoodsListAO.java
@@ -0,0 +1,15 @@
+package com.ft.br.model.ao;
+
+import com.ft.db.model.PageParam;
+
+/**
+ * GoodsListAO
+ *
+ * @author shichunyang
+ */
+public class GoodsListAO extends PageParam {
+	/**
+	 * 分类
+	 */
+	private Integer category;
+}
