89e1587193e0668c9942aadc6cd8f665589d94ff
shichunyang <shichunyang@guazi.com>
2019-07-20 16:59:43 +0800

    UserMapper.java

--- src/main/java/com/ft/br/dao/UserMapper.java
+++ src/main/java/com/ft/br/dao/UserMapper.java
@@ -1,11 +1,11 @@
 package com.ft.br.dao;
 
+import com.ft.dao.stock.mapper.UserDOMapper;
 import com.ft.dao.stock.model.UserDO;
-import com.ft.db.constant.DbConstant;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 /**
  * 登录用户操作类
@@ -14,7 +14,7 @@ import java.util.List;
  */
 @Mapper
 @Component
-public interface UserMapper {
+public interface UserMapper extends UserDOMapper {
 
 	/**
 	 * 根据用户名和密码查找用户
@@ -27,51 +27,20 @@ public interface UserMapper {
 	UserDO getUserByUserNameAndPassword(@Param("username") String username, @Param("password") String password);
 
 	/**
-	 * 根据主键查询用户
-	 *
-	 * @param id 主键
-	 * @return 用户信息
-	 */
-	@Select("select * from `user` where id = #{id}")
-	UserDO getUserById(@Param("id") long id);
-
-	/**
 	 * 根据用户名查询用户信息
 	 *
 	 * @param username 用户名
 	 * @return 用户信息
 	 */
 	@Select("select * from `user` where username = #{username} limit 1")
-	UserDO getUserByUserName(@Param("username") String username);
+	UserDO getUserByUserName(String username);
 
 	/**
-	 * 修改用户密码
+	 * 死锁测试
 	 *
-	 * @param id       用户ID
-	 * @param password 用户密码(md5)
-	 * @return 1: 修改成功
+	 * @param id 主键
+	 * @return 用户信息
 	 */
-	@Update("update `user` set password = #{password} where id = #{id}")
-	int update(@Param("id") long id, @Param("password") String password);
-
-	/**
-	 * 插入用户信息
-	 *
-	 * @param userDO 用户对象
-	 * @return 1: 插入成功
-	 */
-	@Insert("insert into `user` (`username`, `password`) values (#{username}, #{password})")
-	@SelectKey(statement = DbConstant.SELECT_LAST_INSERT_ID, keyColumn = "id", keyProperty = "id", resultType = Long.class, before = false)
-	int insert(UserDO userDO);
-
-	/**
-	 * 查询所有用户信息
-	 *
-	 * @return 所有用户信息
-	 */
-	@Select("select * from `user`")
-	List<UserDO> selectAllUsers();
-
 	@Select("select * from `user` where id = #{id} for update")
-	UserDO deadLock(@Param("id") long id);
+	UserDO deadLock(int id);
 }
