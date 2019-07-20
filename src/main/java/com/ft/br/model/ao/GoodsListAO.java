53ad409dc99cb473593cb4be3b5682604cb77226
shichunyang <shichunyang@guazi.com>
2019-07-20 12:39:15 +0800

    GoodsMapper.java

--- src/main/java/com/ft/br/model/ao/GoodsListAO.java
+++ src/main/java/com/ft/br/model/ao/GoodsListAO.java
@@ -1,12 +1,16 @@
 package com.ft.br.model.ao;
 
 import com.ft.db.model.PageParam;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * GoodsListAO
  *
  * @author shichunyang
  */
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class GoodsListAO extends PageParam {
 	/**
 	 * 分类
