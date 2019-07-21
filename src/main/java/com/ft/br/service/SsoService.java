8c89da235e5dd7f509394fa5665ced07facf4be0
shichunyang <shichunyang@guazi.com>
2019-07-21 15:07:43 +0800

    SsoService.java

--- src/main/java/com/ft/br/service/SsoService.java
+++ src/main/java/com/ft/br/service/SsoService.java
@@ -1,6 +1,8 @@
 package com.ft.br.service;
 
+import com.ft.br.model.ao.LoginAO;
 import com.ft.br.model.bo.CodeBO;
+import com.ft.br.model.bo.TokenBO;
 
 /**
  * SsoService
@@ -14,4 +16,12 @@ public interface SsoService {
 	 * @return 图片验证码
 	 */
 	CodeBO getCode();
+
+	/**
+	 * 登陆
+	 *
+	 * @param loginAO 登录录入数据
+	 * @return sso token
+	 */
+	TokenBO login(LoginAO loginAO);
 }
--- src/main/java/com/ft/br/service/impl/SsoServiceImpl.java
+++ src/main/java/com/ft/br/service/impl/SsoServiceImpl.java
@@ -1,12 +1,19 @@
 package com.ft.br.service.impl;
 
 import com.ft.br.constant.LoginConstant;
+import com.ft.br.model.ao.LoginAO;
 import com.ft.br.model.bo.CodeBO;
+import com.ft.br.model.bo.TokenBO;
 import com.ft.br.service.SsoService;
 import com.ft.redis.base.ValueOperationsCache;
 import com.ft.util.CommonUtil;
 import com.ft.util.ImageUtil;
+import com.ft.util.LogUtil;
 import com.ft.util.StringUtil;
+import com.ft.util.exception.FtException;
+import com.ft.util.model.LogAO;
+import com.ft.util.model.LogBO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -20,6 +27,7 @@ import java.util.concurrent.TimeUnit;
  *
  * @author shichunyang
  */
+@Slf4j
 @Service(value = "com.ft.br.service.impl.SsoServiceImpl")
 public class SsoServiceImpl implements SsoService {
 
@@ -46,5 +54,39 @@ public class SsoServiceImpl implements SsoService {
 		valueOperationsCache.setIfAbsent(codeIdKey, code, 300_000L, TimeUnit.MILLISECONDS);
 
 		return codeBO;
+	}
+
+	@Override
+	public TokenBO login(LoginAO loginAO) {
+		// 校验code
+		String errorMessage = this.checkCode(loginAO);
+		if (errorMessage != null) {
+			FtException.throwException(errorMessage);
+		}
+		return null;
+	}
+
+	private String checkCode(LoginAO loginAO) {
+		String errorMessage = "验证码不正确";
+
+		// 校验code
+		String codeId = loginAO.getCodeId();
+		String redisCode = valueOperationsCache.get(StringUtil.append(StringUtil.REDIS_SPLIT, LoginConstant.REDIS_VERIFICATION_CODE, codeId));
+		if (StringUtil.isNull(redisCode)) {
+			LogBO logBO = LogUtil.log("验证码不存在或已过期",
+					LogAO.build("codeId", codeId));
+			log.info(logBO.getLogPattern(), logBO.getParams());
+			return errorMessage;
+		}
+
+		if (!redisCode.equalsIgnoreCase(loginAO.getCode())) {
+			LogBO logBO = LogUtil.log("验证码比对不一致",
+					LogAO.build("code", loginAO.getCode()),
+					LogAO.build("redisCode", redisCode));
+			log.info(logBO.getLogPattern(), logBO.getParams());
+			return errorMessage;
+		}
+
+		return null;
 	}
 }
