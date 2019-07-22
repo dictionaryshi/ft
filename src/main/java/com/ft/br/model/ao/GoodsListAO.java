35c297269ca52f8f85aa1d5a0a06ef53d9fad787
shichunyang <shichunyang@guazi.com>
2019-07-22 17:17:16 +0800

    GoodsListAO.java

--- src/main/java/com/ft/br/model/ao/GoodsListAO.java
+++ src/main/java/com/ft/br/model/ao/GoodsListAO.java
@@ -1,19 +1,26 @@
 package com.ft.br.model.ao;
 
 import com.ft.db.model.PageParam;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * GoodsListAO
  *
  * @author shichunyang
  */
-@EqualsAndHashCode(callSuper = true)
-@Data
+@ApiModel("分页查询商品信息")
+@Getter
+@Setter
+@ToString
 public class GoodsListAO extends PageParam {
-	/**
-	 * 分类
-	 */
-	private Integer category;
+
+	@NotNull(message = "分类id不为null")
+	@ApiModelProperty(value = "分类id", required = true, example = "10")
+	private Integer categoryId;
 }
