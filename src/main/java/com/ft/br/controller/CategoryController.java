08a79401981c3f8741ecb8e9f48c4fa76990a78d
shichunyang <shichunyang@guazi.com>
2019-07-22 10:55:30 +0800

    CategoryController.java

--- src/main/java/com/ft/br/controller/CategoryController.java
+++ src/main/java/com/ft/br/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.ft.br.controller;
 
+import com.ft.br.model.ao.CategoryGetAO;
 import com.ft.br.model.bo.CategoryBO;
 import com.ft.br.service.CategoryService;
 import com.ft.br.service.impl.CategoryServiceImpl;
@@ -12,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -42,6 +44,19 @@ public class CategoryController {
 	@GetMapping("/all")
 	public RestResult<List<CategoryBO>> all() {
 		return RestResult.success(categoryService.listAllCategories());
+	}
+
+	/**
+	 * 根据主键查询分类信息
+	 */
+	@ApiOperation("根据id查询分类信息")
+	@LoginCheck
+	@GetMapping("/get")
+	public RestResult<CategoryBO> get(
+			@Valid CategoryGetAO categoryGetAO
+	) {
+		CategoryBO categoryBO = categoryService.getById(categoryGetAO.getId());
+		return RestResult.success(categoryBO);
 	}
 
 	@ApiOperation("添加分类信息")
@@ -94,21 +109,5 @@ public class CategoryController {
 		Map<String, Object> result = new HashMap<>(16);
 		result.put("flag", flag);
 		return JsonUtil.object2Json(RestResult.success(result));
-	}
-
-	@ApiOperation("根据id查询分类信息")
-	@ApiImplicitParams({
-			@ApiImplicitParam(name = "id", value = "分类id", required = true, dataType = SwaggerConstant.DATA_TYPE_INT, paramType = SwaggerConstant.PARAM_TYPE_QUERY, example = "0"),
-	})
-	/**
-	 * 根据主键查询分类信息
-	 *
-	 * @param id 分类主键
-	 * @return 分类信息
-	 */
-	@RequestMapping(value = "/get", method = RequestMethod.POST)
-	@LoginCheck
-	public String get(@RequestParam(value = "id") short id) {
-		return JsonUtil.object2Json(RestResult.success(categoryServiceImpl.get(id)));
 	}
 }
