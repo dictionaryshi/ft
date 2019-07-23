fed698acc48a7b9a49ecf81fd70759a717b448c0
shichunyang <shichunyang@guazi.com>
2019-07-23 10:48:10 +0800

    UserServiceImpl.java

---
+++ src/main/java/com/ft/br/service/impl/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.ft.br.service.impl;
+
+import com.ft.br.dao.UserMapper;
+import com.ft.br.service.UserService;
+import com.ft.dao.stock.model.UserDO;
+import com.ft.util.ObjectUtil;
+import com.ft.util.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * UserServiceImpl
+ *
+ * @author shichunyang
+ */
+@Service("com.ft.br.service.impl.UserServiceImpl")
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserMapper userMapper;
+
+	@Override
+	public Map<Integer, String> listUserNamesByIds(List<Integer> ids) {
+		Map<Integer, String> resultMap = new HashMap<>(16);
+
+		if (ObjectUtil.isEmpty(ids)) {
+			return resultMap;
+		}
+
+		String idStr = StringUtil.join(ids, ",");
+		Map<Integer, UserDO> userDOMap = userMapper.selectByIds(idStr);
+		userDOMap.forEach((id, userDO) -> resultMap.put(id, userDO.getUsername()));
+
+		return resultMap;
+	}
+}
