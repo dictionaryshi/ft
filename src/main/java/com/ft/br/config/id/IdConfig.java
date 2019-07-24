0d35c17fe4b9347f88b7b060d04a741e5b102cf6
shichunyang <shichunyang@guazi.com>
2019-07-24 19:35:03 +0800

    IdConfig.java

---
+++ src/main/java/com/ft/br/config/id/IdConfig.java
@@ -0,0 +1,19 @@
+package com.ft.br.config.id;
+
+import com.ft.util.plugin.Snowflake;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * IdConfig
+ *
+ * @author shichunyang
+ */
+@Configuration("com.ft.br.config.id.IdConfig")
+public class IdConfig {
+
+	@Bean("com.ft.util.plugin.Snowflake")
+	public Snowflake snowflake() {
+		return new Snowflake(0);
+	}
+}
