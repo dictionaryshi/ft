97d42d9302d911a66ed6a52aa4f151b1034583c3
shichunyang <shichunyang@guazi.com>
2019-07-25 13:09:36 +0800

    OrderService.java

--- src/main/java/com/ft/br/service/OrderService.java
+++ src/main/java/com/ft/br/service/OrderService.java
@@ -1,9 +1,18 @@
 package com.ft.br.service;
 
+import com.ft.br.model.ao.order.OrderAddAO;
+
 /**
  * OrderService
  *
  * @author shichunyang
  */
 public interface OrderService {
+	/**
+	 * 创建订单
+	 *
+	 * @param orderAddAO 订单信息
+	 * @return true 创建成功
+	 */
+	boolean createOrder(OrderAddAO orderAddAO);
 }
