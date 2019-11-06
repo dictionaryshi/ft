67ac0702754bd721c314f578d85d79484e24f7d0
shichunyang <shichunyang@guazi.com>
2019-11-06 20:59:50 +0800

    RemoteService.java

--- src/main/java/com/ft/br/feign/RemoteService.java
+++ src/main/java/com/ft/br/feign/RemoteService.java
@@ -1,26 +1,13 @@
 package com.ft.br.feign;
 
-import com.ft.rpc.api.model.RpcParam;
-import com.ft.rpc.api.model.RpcResult;
-import com.ft.util.model.RestResult;
+import com.ft.rpc.api.service.FeignService;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
 
 /**
  * 远程service
  *
  * @author shichunyang
  */
-@FeignClient(name = "feign", fallbackFactory = RemoteServiceFallbackFactory.class, path = RestResult.API + "/rpc")
-public interface RemoteService {
-
-	@PutMapping("/put")
-	RestResult<RpcResult> put(
-			@RequestBody RpcParam rpcParam
-	);
-
-	@GetMapping("/get")
-	RestResult<RpcResult> get(
-			RpcParam rpcParam
-	);
+@FeignClient(name = "feign", fallbackFactory = RemoteServiceFallbackFactory.class)
+public interface RemoteService extends FeignService {
 }
