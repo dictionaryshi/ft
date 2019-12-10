d1ca7634e16bb44ccebfb1c2a46133cccec9bc44
shichunyang <shichunyang@guazi.com>
2019-12-10 09:45:44 +0800

    GC2

---
+++ src/main/java/com/ft/br/study/gc/GC2.java
@@ -0,0 +1,15 @@
+package com.ft.br.study.gc;
+
+/**
+ * GC2
+ *
+ * @author shichunyang
+ */
+public class GC2 {
+
+	/**
+	 * -Xms20m -Xmx20m -Xmn10m -Xss256k -XX:SurvivorRatio=8 -XX:PretenureSizeThreshold=10485760 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:/data/gc/gc_ft_%t-log
+	 */
+	public static void main(String[] args) {
+	}
+}
