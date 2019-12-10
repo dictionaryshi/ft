c3963a8560b0c163006bfd1e850ab6c157f3bc06
shichunyang <shichunyang@guazi.com>
2019-12-10 10:21:59 +0800

    GC2.java

--- src/main/java/com/ft/br/study/gc/GC2.java
+++ src/main/java/com/ft/br/study/gc/GC2.java
@@ -12,5 +12,20 @@ public class GC2 {
 	 * 给堆内存分配20MB内存空间, 其中新生代是10MB内存空间, 其中Eden区占8MB, 每个Survivor区占1MB, 大对象必须超过10MB才会直接进入老年代。
 	 */
 	public static void main(String[] args) {
+		byte[] array1 = new byte[2 * 1024 * 1024];
+		array1 = new byte[2 * 1024 * 1024];
+		array1 = new byte[2 * 1024 * 1024];
+		array1 = null;
+
+		byte[] array2 = new byte[128 * 1024];
+
+		byte[] array3 = new byte[2 * 1024 * 1024];
+
+		array3 = new byte[2 * 1024 * 1024];
+		array3 = new byte[2 * 1024 * 1024];
+		array3 = new byte[128 * 1024];
+		array3 = null;
+
+		byte[] array4 = new byte[2 * 1024 * 1024];
 	}
 }
