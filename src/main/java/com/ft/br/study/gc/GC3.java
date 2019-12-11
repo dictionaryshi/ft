68e979d9165acd005d725e9c6255f6342c404902
shichunyang <shichunyang@guazi.com>
2019-12-11 11:34:06 +0800

    GC3.java

--- src/main/java/com/ft/br/study/gc/GC3.java
+++ src/main/java/com/ft/br/study/gc/GC3.java
@@ -12,6 +12,7 @@ public class GC3 {
 	 * 给堆内存分配20MB内存空间, 其中新生代是10MB内存空间, 其中Eden区占8MB, 每个Survivor区占1MB, 大对象必须超过3MB才会直接进入老年代。
 	 */
 	public static void main(String[] args) {
+		// 大对象直接在老年代分配
 		byte[] array1 = new byte[4 * 1024 * 1024];
 		array1 = null;
 
@@ -20,6 +21,7 @@ public class GC3 {
 		byte[] array4 = new byte[2 * 1024 * 1024];
 		byte[] array5 = new byte[500 * 1024];
 
+		// array6在Eden创建前, 发生一次YGC。
 		byte[] array6 = new byte[2 * 1024 * 1024];
 	}
 }
