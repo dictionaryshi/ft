7271834cb515a701c839559ae5d88399b75fb43c
shichunyang <shichunyang@guazi.com>
2019-12-11 17:26:43 +0800

    GC3.java

--- src/main/java/com/ft/br/study/gc/GC3.java
+++ src/main/java/com/ft/br/study/gc/GC3.java
@@ -25,3 +25,15 @@ public class GC3 {
 		byte[] array6 = new byte[2 * 1024 * 1024];
 	}
 }
+/*
+CommandLine flags: -XX:InitialHeapSize=20971520 -XX:MaxHeapSize=20971520 -XX:MaxNewSize=10485760 -XX:MaxTenuringThreshold=15 -XX:NewSize=10485760 -XX:OldPLABSize=16 -XX:PretenureSizeThreshold=3145728 -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:SurvivorRatio=8 -XX:ThreadStackSize=256 -XX:+UseCompressedClassPointers -XX:+UseCompressedOops -XX:+UseConcMarkSweepGC -XX:+UseParNewGC
+0.083: [GC (Allocation Failure) 0.083: [ParNew (promotion failed): 7152K->7953K(9216K), 0.0123063 secs]0.095: [CMS: 8194K->6925K(10240K), 0.0024515 secs] 11248K->6925K(19456K), [Metaspace: 2621K->2621K(1056768K)], 0.0148549 secs] [Times: user=0.04 sys=0.00, real=0.01 secs]
+Heap
+ par new generation   total 9216K, used 2212K [0x00000007bec00000, 0x00000007bf600000, 0x00000007bf600000)
+  eden space 8192K,  27% used [0x00000007bec00000, 0x00000007bee290e0, 0x00000007bf400000)
+  from space 1024K,   0% used [0x00000007bf500000, 0x00000007bf500000, 0x00000007bf600000)
+  to   space 1024K,   0% used [0x00000007bf400000, 0x00000007bf400000, 0x00000007bf500000)
+ concurrent mark-sweep generation total 10240K, used 6925K [0x00000007bf600000, 0x00000007c0000000, 0x00000007c0000000)
+ Metaspace       used 2628K, capacity 4486K, committed 4864K, reserved 1056768K
+  class space    used 286K, capacity 386K, committed 512K, reserved 1048576K
+ */
