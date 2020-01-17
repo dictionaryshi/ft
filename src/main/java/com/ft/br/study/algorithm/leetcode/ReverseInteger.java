402a9c8923bcd02384d95751252eb5b0be2acbbe
shichunyang <shichunyang@guazi.com>
2020-01-17 14:59:57 +0800

    ReverseInteger.java

--- src/main/java/com/ft/br/study/algorithm/leetcode/ReverseInteger.java
+++ src/main/java/com/ft/br/study/algorithm/leetcode/ReverseInteger.java
@@ -24,13 +24,39 @@ public class ReverseInteger {
 		return rev;
 	}
 
+	/**
+	 * 字符串转数字
+	 */
 	public static int convert(String str) {
-		int result = 0;
-		char[] chars = str.toCharArray();
-		for (char ch : chars) {
-			result = result * 10 + (ch - '0');
+		if (str == null || str.length() == 0) {
+			return 0;
 		}
-		return result;
+		str = str.trim();
+
+		char firstChar = str.charAt(0);
+		int sign = 1;
+		int start = 0;
+		long res = 0;
+		if (firstChar == '+') {
+			start++;
+		} else if (firstChar == '-') {
+			sign = -1;
+			start++;
+		}
+
+		for (int i = start; i < str.length(); i++) {
+			if (!Character.isDigit(str.charAt(i))) {
+				return (int) res * sign;
+			}
+			res = res * 10 + str.charAt(i) - '0';
+			if (sign == 1 && res > Integer.MAX_VALUE) {
+				return Integer.MAX_VALUE;
+			}
+			if (sign == -1 && res > Integer.MAX_VALUE) {
+				return Integer.MIN_VALUE;
+			}
+		}
+		return (int) res * sign;
 	}
 
 	/**
