eba210d693bfadc4566d3761d27a0641fb524db1
shichunyang <shichunyang@guazi.com>
2020-01-17 15:25:10 +0800

    ReverseInteger.java

--- src/main/java/com/ft/br/study/algorithm/leetcode/ReverseInteger.java
+++ src/main/java/com/ft/br/study/algorithm/leetcode/ReverseInteger.java
@@ -19,6 +19,14 @@ public class ReverseInteger {
 			int pop = x % 10;
 			// 十位
 			x /= 10;
+
+			if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+				return 0;
+			}
+			if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+				return 0;
+			}
+
 			rev = rev * 10 + pop;
 		}
 		return rev;
