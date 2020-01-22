622ce36b8251c9667de881fb8c3f9efb42620ecf
shichunyang <shichunyang@guazi.com>
2020-01-22 21:20:26 +0800

    堆排序

--- src/main/java/com/ft/br/study/algorithm/leetcode/Sort.java
+++ src/main/java/com/ft/br/study/algorithm/leetcode/Sort.java
@@ -62,4 +62,51 @@ public class Sort {
 		}
 		return result;
 	}
+
+	/**
+	 * 堆排序
+	 */
+	public List<Integer> sortArray3(int[] nums) {
+		int size = nums.length;
+		int[] heap = new int[size + 1];
+		for (int i = 0; i < size; i++) {
+			heap[i + 1] = nums[i];
+		}
+		// 构造堆
+		for (int parentIndex = size / 2; parentIndex >= 1; parentIndex--) {
+			sink(heap, parentIndex, size);
+		}
+
+		// 下沉排序
+		while (size > 1) {
+			// 将最大元素放置数组最后, 并逻辑移除
+			exchange(heap, 1, size--);
+			// 将新堆使用下沉恢复
+			sink(heap, 1, size);
+		}
+
+		List<Integer> result = new ArrayList<>();
+		for (int i = 1; i < heap.length; i++) {
+			result.add(heap[i]);
+		}
+		return result;
+	}
+
+	private static void sink(int[] arr, int parentIndex, int size) {
+		while (2 * parentIndex <= size) {
+			// 左结点
+			int childIndex = 2 * parentIndex;
+			if (childIndex < size && arr[childIndex] < arr[childIndex + 1]) {
+				// 右节点
+				childIndex++;
+			}
+			if (arr[parentIndex] > arr[childIndex]) {
+				break;
+			}
+
+			exchange(arr, parentIndex, childIndex);
+
+			parentIndex = childIndex;
+		}
+	}
 }
