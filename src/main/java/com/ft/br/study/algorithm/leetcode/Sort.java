b82428c68ae68f66720586a432d7be03221e888b
shichunyang <shichunyang@guazi.com>
2020-01-22 20:07:28 +0800

    冒泡排序

--- src/main/java/com/ft/br/study/algorithm/leetcode/Sort.java
+++ src/main/java/com/ft/br/study/algorithm/leetcode/Sort.java
@@ -39,4 +39,36 @@ public class Sort {
 		}
 		return result;
 	}
+
+	/**
+	 * 冒泡排序
+	 */
+	public List<Integer> sortArray2(int[] nums) {
+		int length = nums.length;
+		// 剩下的最后一个数不需要比较
+		for (int i = 0; i < length - 1; i++) {
+
+			// 本次存储最大元素的索引
+			int maxIndex = length - 1 - i;
+
+			// 最大元素所在的索引
+			int index = 0;
+
+			for (int j = 0; j < length - 1 - i; j++) {
+				if (nums[j + 1] < nums[j]) {
+					if (nums[index] < nums[j]) {
+						index = j;
+					}
+				}
+			}
+
+			exchange(nums, index, maxIndex);
+		}
+
+		List<Integer> result = new ArrayList<>();
+		for (int num : nums) {
+			result.add(num);
+		}
+		return result;
+	}
 }
