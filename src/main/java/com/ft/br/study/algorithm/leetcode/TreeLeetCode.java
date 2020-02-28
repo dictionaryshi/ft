5a82e3656ed09742cb0b11f01dcb1f75c2551dd4
shichunyang <shichunyang@guazi.com>
2020-02-28 09:30:40 +0800

    TreeLeetCode.java

--- src/main/java/com/ft/br/study/algorithm/leetcode/TreeLeetCode.java
+++ src/main/java/com/ft/br/study/algorithm/leetcode/TreeLeetCode.java
@@ -186,4 +186,23 @@ public class TreeLeetCode {
 		ans = Math.max(ans, arrowLeft + arrowRight);
 		return Math.max(arrowLeft, arrowRight);
 	}
+
+	/**
+	 * 给定一棵二叉树, 计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过根结点。
+	 */
+	public int diameterOfBinaryTree(TreeNode root) {
+		ans = 1;
+		depth(root);
+		return ans - 1;
+	}
+
+	public int depth(TreeNode node) {
+		if (node == null) {
+			return 0;
+		}
+		int L = depth(node.left);
+		int R = depth(node.right);
+		ans = Math.max(ans, L + R + 1);
+		return Math.max(L, R) + 1;
+	}
 }
