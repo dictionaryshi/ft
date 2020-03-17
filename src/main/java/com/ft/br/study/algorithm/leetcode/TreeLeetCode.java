cb9ce0a2008365fc9ab270fa65fe7c23796d36ad
shichunyang <shichunyang@guazi.com>
2020-03-17 13:00:25 +0800

    TreeLeetCode.java

--- src/main/java/com/ft/br/study/algorithm/leetcode/TreeLeetCode.java
+++ src/main/java/com/ft/br/study/algorithm/leetcode/TreeLeetCode.java
@@ -205,4 +205,19 @@ public class TreeLeetCode {
 		ans = Math.max(ans, L + R + 1);
 		return Math.max(L, R) + 1;
 	}
+
+	/**
+	 * 给定一个二叉树和一个目标和, 判断该树中是否存在根节点到叶子节点的路径, 这条路径上所有节点值相加等于目标和。
+	 */
+	public boolean hasPathSum(TreeNode root, int sum) {
+		if (root == null) {
+			return false;
+		}
+
+		sum -= root.val;
+		if ((root.left == null) && (root.right == null)) {
+			return (sum == 0);
+		}
+		return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+	}
 }
