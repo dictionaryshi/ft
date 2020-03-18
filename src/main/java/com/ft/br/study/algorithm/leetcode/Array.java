a566aadef1b0fcb435939a73fc92dc06c7ad1ccf
shichunyang <shichunyang@guazi.com>
2020-03-18 12:54:40 +0800

    Array.java

--- src/main/java/com/ft/br/study/algorithm/leetcode/Array.java
+++ src/main/java/com/ft/br/study/algorithm/leetcode/Array.java
@@ -1,6 +1,6 @@
 package com.ft.br.study.algorithm.leetcode;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * 数组类题目
@@ -168,5 +168,38 @@ public class Array {
 			}
 		}
 		return i;
+	}
+
+	/**
+	 * 数组求和, 每个元素可以使用多次
+	 */
+	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+		List<List<Integer>> res = new ArrayList<>();
+		Arrays.sort(candidates);
+		dfs(candidates, candidates.length, target, 0, new ArrayList<>(), res);
+		return res;
+	}
+
+	private void dfs(int[] candidates,
+					 int len,
+					 int residue,
+					 int begin,
+					 ArrayList<Integer> path,
+					 List<List<Integer>> res) {
+		if (residue == 0) {
+			res.add(new ArrayList<>(path));
+			return;
+		}
+
+		for (int i = begin; i < len; i++) {
+			if (residue - candidates[i] < 0) {
+				break;
+			}
+
+			path.add(candidates[i]);
+			dfs(candidates, len, residue - candidates[i], i, path, res);
+			path.remove(path.size() - 1);
+
+		}
 	}
 }
