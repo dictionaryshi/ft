7cedba188a48657bef153f347666d8c01301ec36
shichunyang <shichunyang@guazi.com>
2020-03-27 13:27:29 +0800

    PriorityQueue.java

---
+++ src/main/java/com/ft/br/study/source/PriorityQueue.java
@@ -0,0 +1,112 @@
+package com.ft.br.study.source;
+
+import java.util.Arrays;
+
+@SuppressWarnings("unchecked")
+public class PriorityQueue<E> {
+
+	transient int modCount = 0;
+
+	private int size = 0;
+
+	transient Object[] queue;
+
+	private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+
+	public boolean offer(E e) {
+		if (e == null) {
+			throw new NullPointerException();
+		}
+		modCount++;
+		int i = size;
+		if (i >= queue.length) {
+			grow(i + 1);
+		}
+		size = i + 1;
+		if (i == 0) {
+			queue[0] = e;
+		} else {
+			siftUp(i, e);
+		}
+		return true;
+	}
+
+	@SuppressWarnings("unchecked")
+	public E poll() {
+		if (size == 0) {
+			return null;
+		}
+		int s = --size;
+		modCount++;
+		E result = (E) queue[0];
+		E x = (E) queue[s];
+		queue[s] = null;
+		if (s != 0) {
+			siftDown(0, x);
+		}
+		return result;
+	}
+
+	private void grow(int minCapacity) {
+		int oldCapacity = queue.length;
+		int newCapacity = oldCapacity + ((oldCapacity < 64) ?
+				(oldCapacity + 2) :
+				(oldCapacity >> 1));
+		if (newCapacity - MAX_ARRAY_SIZE > 0) {
+			newCapacity = hugeCapacity(minCapacity);
+		}
+		queue = Arrays.copyOf(queue, newCapacity);
+	}
+
+	private static int hugeCapacity(int minCapacity) {
+		if (minCapacity < 0) {
+			throw new OutOfMemoryError();
+		}
+		return (minCapacity > MAX_ARRAY_SIZE) ?
+				Integer.MAX_VALUE :
+				MAX_ARRAY_SIZE;
+	}
+
+	private void siftUp(int k, E x) {
+		siftUpComparable(k, x);
+	}
+
+	@SuppressWarnings("unchecked")
+	private void siftUpComparable(int k, E x) {
+		Comparable<? super E> key = (Comparable<? super E>) x;
+		while (k > 0) {
+			int parent = (k - 1) >>> 1;
+			Object e = queue[parent];
+			if (key.compareTo((E) e) >= 0) {
+				break;
+			}
+			queue[k] = e;
+			k = parent;
+		}
+		queue[k] = key;
+	}
+
+	private void siftDown(int k, E x) {
+		siftDownComparable(k, x);
+	}
+
+	private void siftDownComparable(int k, E x) {
+		Comparable<? super E> key = (Comparable<? super E>) x;
+		int half = size >>> 1;
+		while (k < half) {
+			int child = (k << 1) + 1;
+			Object c = queue[child];
+			int right = child + 1;
+			if (right < size &&
+					((Comparable<? super E>) c).compareTo((E) queue[right]) > 0) {
+				c = queue[child = right];
+			}
+			if (key.compareTo((E) c) <= 0) {
+				break;
+			}
+			queue[k] = c;
+			k = child;
+		}
+		queue[k] = key;
+	}
+}
