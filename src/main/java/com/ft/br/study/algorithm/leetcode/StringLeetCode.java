ef2f96e03a1b0e23ec08cb582f53fb06050e83f4
shichunyang <shichunyang@guazi.com>
2020-04-03 08:59:14 +0800

    StringLeetCode.java

--- src/main/java/com/ft/br/study/algorithm/leetcode/StringLeetCode.java
+++ src/main/java/com/ft/br/study/algorithm/leetcode/StringLeetCode.java
@@ -26,37 +26,6 @@ public class StringLeetCode {
 		return prefix;
 	}
 
-	public String longestCommonPrefix2(String[] strs) {
-		if (strs == null || strs.length == 0) {
-			return "";
-		}
-		int minLen = Integer.MAX_VALUE;
-		for (String str : strs) {
-			minLen = Math.min(minLen, str.length());
-		}
-		int low = 1;
-		int high = minLen;
-		while (low <= high) {
-			int middle = (low + high) / 2;
-			if (isCommonPrefix(strs, middle)) {
-				low = middle + 1;
-			} else {
-				high = middle - 1;
-			}
-		}
-		return strs[0].substring(0, (low + high) / 2);
-	}
-
-	private boolean isCommonPrefix(String[] strs, int len) {
-		String prefix = strs[0].substring(0, len);
-		for (int i = 1; i < strs.length; i++) {
-			if (!strs[i].startsWith(prefix)) {
-				return false;
-			}
-		}
-		return true;
-	}
-
 	/**
 	 * 字符串关键字加粗
 	 */
