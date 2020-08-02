bfc79a7bb3bbeecafa3418d5414a20559f7afb41
史春阳 <shichunyang02@meituan.com>
2020-08-02 08:45:25 +0800

    mapstruct

---
+++ src/main/java/com/ft/br/mapstruct/Amo.java
@@ -0,0 +1,22 @@
+package com.ft.br.mapstruct;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+/**
+ * Created by shichunyang on 2020/8/1.
+ */
+@Getter
+@Setter
+@ToString
+public class Amo {
+
+    private int alId;
+
+    private String alName;
+
+    private Date alDate;
+}
---
+++ src/main/java/com/ft/br/mapstruct/Bmo.java
@@ -0,0 +1,24 @@
+package com.ft.br.mapstruct;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Created by shichunyang on 2020/8/1.
+ */
+@Getter
+@Setter
+@ToString
+public class Bmo {
+
+    private int blId;
+
+    private String blName;
+
+    private String blDate;
+
+    private int blSum;
+
+    private String blbb;
+}
