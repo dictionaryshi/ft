cf6f35fa366647bd3d9a846602d2a7f18f00a9a2
shichunyang02 <shichunyang02@meituan.com>
2024-01-03 12:08:35 +0800

    删除无用代吗

--- src/main/java/com/ft/br/config/ProFileConfig.java
+++ src/main/java/com/ft/br/config/ProFileConfig.java
@@ -1,12 +1,6 @@
 package com.ft.br.config;
 
 import com.ft.br.service.impl.SpringLifeServiceImpl;
-import com.ft.dao.stock.model.UserDO;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Scope;
 
 /**
  * 多环境配置
@@ -16,31 +10,8 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class ProFileConfig {
 
-    /**
-     * 获取User对象
-     * Scope 多实例时, 启动容器时不会创建该对象
-     * Lazy 单实例时, 添加该注解, 启动容器不会创建该对象
-     *
-     * @return User对象
-     */
-    @Bean
-    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
-    @Lazy
-    public UserDO scy() {
-        UserDO user = new UserDO();
-        user.setUsername("scy");
-        return user;
-    }
-
     @Bean(initMethod = "init", destroyMethod = "customDestroy")
     public SpringLifeServiceImpl springLifeService() {
         return new SpringLifeServiceImpl("SpringLifeBean");
-    }
-
-    @Bean
-    public UserDO zgl() {
-        UserDO user = new UserDO();
-        user.setUsername("zgl");
-        return user;
     }
 }
